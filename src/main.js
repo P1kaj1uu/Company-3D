f7e2e39a4ea0eef08a926c0f585b15523f7acf51
贰十一 <891523233@qq.com>
2022-12-29 16:30:54 +0800

    打包部署去掉打印内容

--- src/main.js
+++ src/main.js
@@ -8,5 +8,11 @@ import animated from 'animate.css'
 import './components/loading/loading.css'
 // 字体
 import '@/assets/font/index.css'
+if (process.env.NODE_ENV !== 'development') {
+  // process是Node环境全部变量, 运行时根据敲击的命令不同, 脚手架会取环境变量给env添加属性和值
+  console.log = function () {}
+  console.error = function () {}
+  console.dir = function () {}
+}
 const app = createApp(App)
 app.use(animated).use(store).use(router).mount('#app')
